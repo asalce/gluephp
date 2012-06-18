11fc8a92a3240267ae374d1be9baff32d83282d9
Max Timkovich <mtimkovich@gmail.com>
2012-06-17 21:14:49 -0500

    Added index.php file

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+include_once 'glue.php';
+
+$urls = array(
+    '/(.*)' => 'Index',
+);
+
+class Index {
+    public function GET($params) {
+        $name = 'World';
+        if (!empty($params[0])) {
+            $name = $params[0];
+            $name = ucfirst($name);
+        }
+
+        echo "Hello, $name!";
+    }
+}
+
+glue::stick($urls);
+
+?>
